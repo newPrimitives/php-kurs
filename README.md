921d4bdf00b22189c4c224386155347ae5610a55
Nermin Šehić <nerminsehic1993@gmail.com>
2015-10-07 23:18:24 +0200

    Added operations

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<h1> PHP Kurs za pocetnike - Academy397 </h1>
+<h1> PHP Kurs za pocetnike - Academy387 </h1>
 Repozitory sadrzi fajlove vezane za kurs PHP-a za pocetnike na Academy387. Cilj kursa je da po njegovom završetku, svako od polaznika ima znanje da samostalno isprogramira CRUD CMS sistem. Repozitorij se updateuje dva puta sedmicno.
 
 <h1> Dodatni resursi </h1>
@@ -14,7 +14,11 @@ Repozitory sadrzi fajlove vezane za kurs PHP-a za pocetnike na Academy387. Cilj 
 <p> Lekcija 2 ukljucuje shvatanje osnova: sintakse, varijable, artrimetričke operacije, operacije sa stringovima, PHP i HTML combined </p>
 
 <ul> 
+
 	<li> <a href="https://github.com/newPrimitives/php-kurs/blob/master/lecture2/example1_starterTemplate.php"> Starter template za vjezbu 1 </a> </li>
 
 	<li> <a href="https://github.com/newPrimitives/php-kurs/blob/master/lecture2/example1_final.php"> Rjesenje vjezbe 1 </a> </li>
+
+	<li> <a href="https://github.com/newPrimitives/php-kurs/blob/master/lecture2/example1_final.php"> Lista operacija u PHP </a> </li>
+
 </ul>
---
+++ lecture2/phpOperators.php
@@ -0,0 +1,86 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title> PHP operacije </title>
+  <meta name="description" content="PHP Operacije">
+  <meta name="author" content="Academy387">
+
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+  
+</head>
+
+<body>
+
+	<h1> Aritmetricke operacije </h1>	
+	<!-- Sabiranje -->
+	<?php
+	$x = 5; 
+	$y = 7;
+
+	echo "Rezultat sabiranja: ";
+	echo $x + $y;
+	?>
+
+	<br>
+
+	<!-- Oduzimanje -->
+	<?php
+	$x = 10; 
+	$y = 3;
+
+	echo "Rezultat oduzimanja: ";
+	echo $x + $y;
+	?>
+
+	<br>
+	
+	<!-- Mnozenje -->
+	<?php
+	$x = 10; 
+	$y = 3;
+
+	echo "Rezultat mnozenja: ";
+	echo $x * $y;
+	?>
+
+	<br>
+	
+	<!-- Dijeljenje -->
+	<?php
+	$x = 10; 
+	$y = 3;
+
+	echo "Rezultat dijeljenja: ";
+	echo $x / $y;
+	?>
+
+	<br>
+	
+	<!-- Ostatak -->
+	<?php
+	$x = 10; 
+	$y = 3;
+
+	echo "Ostatak dijeljenja: ";
+	echo $x % $y;
+	?>
+
+	<br>
+	
+	<!-- Kvadriranje -->
+	<?php
+	$x = 2; 
+	$y = 3;
+
+	echo "Rezultat kvadriranja: ";
+	echo $x ** $y;
+	?>
+
+</body>
+</html>
+
