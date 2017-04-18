868062f7291d750b3807ac13afd82e9a76ab952e
Nermin Sehic <nermin@Nermins-MacBook-Pro.local>
2017-04-18 13:40:44 +0200

    Updated homework solution

--- lecture7/homework3_solution.php
+++ lecture7/homework3_solution.php
@@ -1,16 +1,60 @@
 <?php
 
-class User 
-{
+class User {
 	
 	public $name;
 	public $dateOfBirth;
 	public $placeOfBirth;
 }
 
-class AcademyStudent extends User{
+class AcademyStudent extends User {
 
 	public $courseName;
 	public $courseLenght;
 
+	public function __construct ($name, $dateOfBirth, $placeOfBirth, $courseName, $courseLenght) {
+
+    	$this->name = $name;
+    	$this->dateOfBirth = $dateOfBirth;
+    	$this->placeOfBirth = $placeOfBirth;
+    	$this->courseName = $courseName;
+    	$this->courseLenght = $courseLenght;
+  	}
 }
+
+function getCourse($courseName) {
+	$courses = [
+		'php za pocetnike' => [
+			new AcademyStudent('Nermin Sehic', '05.12.1993', 'Sarajevo', 'PHP', 15), 
+			new AcademyStudent('John Doe', '01.01.1994', 'Sarajevo', 'PHP', 15)
+		],
+		'javascript' => [
+			new AcademyStudent('Nermin Sehic', '05.12.1993', 'Sarajevo', 'PHP', 15), 
+			new AcademyStudent('John Doe', '01.01.1994', 'Sarajevo', 'PHP', 15)
+		]
+	];
+
+	foreach ($courses as $key => $course) {
+       if ($key == $courseName) {
+           return $course;
+       }
+   }
+
+   return null;
+}
+
+function getStudentsForCourse($courseName) {
+	$students = getCourse($courseName);
+	
+	$result = '<h2> Studenti koji pohadjaju: ' . $courseName . '</h2>';
+	$result .= '<ul>';
+	
+	foreach($students as $student) {
+		$result .= '<li>' . $student->name . '</li>';
+	}
+	
+	$result .= '</ul>';
+	echo $result;
+}
+
+getStudentsForCourse('javascript');
