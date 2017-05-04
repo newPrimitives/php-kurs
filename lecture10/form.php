a34f9cf861e25d51b755e7bb8c6c768bbc62be2f
Nermin Sehic <nermin@Nermins-MacBook-Pro.local>
2017-05-04 16:36:17 +0200

    Updated form example

--- lecture10/form.php
+++ lecture10/form.php
@@ -1,17 +1,24 @@
+<?php 
+  if (isset($_POST['firstname'])) {
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $gender = $_POST['gender'];
+    echo $firstname . ' ' . $lastname . ' ' . $gender;
+  } 
+?>
 
-<form action="joinQuery.php" method="POST">
+<form action="index.php" method="POST">
   <fieldset>
     <legend>Personal information:</legend>
     First name:<br>
-    <input type="text" name="firstname" value="Mickey">
+    <input type="text" name="firstname" value="" placeholder="Upisi svoje ime">
     <br>
     Last name:<br>
     <input type="text" name="lastname" placeholder="Upisi svoje prezime">
     <br><br>
-     <input type="radio" name="sex" value="1" checked>Male
+     <input type="radio" name="gender" value="1" checked>Musko
     <br>
-    <input type="radio" name="sex" value="2">Female <br> <br>
-    <input type="submit" value="Okini">
+    <input type="radio" name="gender" value="2">Zensko <br> <br>
+    <input type="submit" value="Submit">
   </fieldset>
-
 </form>
